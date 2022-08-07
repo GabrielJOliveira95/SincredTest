b42785e14e0c68fb641be7d5e26413fbdcea5bea
Gabriel Oliveira <p22.gabriel@webmotors.com.br>
2022-08-07 17:49:23 -0300

    layout adjusts

--- app/src/main/java/com/example/sincredtest/ui/eventDetail/SincredEventDetailFragment.kt
+++ app/src/main/java/com/example/sincredtest/ui/eventDetail/SincredEventDetailFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.sincredtest.R
 import com.example.sincredtest.constants.CONSTANTS.EVENT_DETAIL_BUNDLE
