b6e8c00b4ec9d5d802b8f162bdb8d347922d7ff4
Gabriel Oliveira <p22.gabriel@webmotors.com.br>
2022-08-08 13:48:48 -0300

    layout adjust

---
+++ app/src/test/java/com/example/sincredtest/ui/SincredViewModelTest.kt
@@ -0,0 +1,11 @@
+package com.example.sincredtest.ui
+
+import com.example.sincredtest.TestCoroutineRule
+import org.junit.Rule
+
+class SincredViewModelTest {
+
+    @get:Rule
+    val testCoroutineRule = TestCoroutineRule()
+
+}
